42062ef41e27780e6d9da577e49a4d8ca2888fe3
manases24 <hugo.orlando2030@gmail.com>
2024-09-09 14:55:25 -0300

    github user search app

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export type AppContextType = {
+  toggleDarkTheme: () => void;
+  isDarkTheme: boolean;
+  //   searchTerm: string;
+  //   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export type AppProviderProps = {
+  children: ReactNode;
+};
